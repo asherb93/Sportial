a0eeafab8b11736ffa09ac19eb6902e69c44070e
Asher Bashian <94224954+asherb93@users.noreply.github.com>
2024-05-04 22:24:39 +0300

    -add profile creation screen

--- app/src/main/java/com/example/sportial/MainActivity.java
+++ app/src/main/java/com/example/sportial/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.sportial;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 
 import androidx.activity.EdgeToEdge;
@@ -21,7 +24,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+         Button btnSignUp = findViewById(R.id.signup_button);
+
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create an Intent object with the target activity class
+                Intent intent = new Intent(MainActivity.this, ProfileCreationActivity.class);
+
+                // Start the SignupActivity
+                startActivity(intent);
+            }
+        });
     }
+
+
+
 
 
 
