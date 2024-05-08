45b32da436f0ceebbb2cdf4fd1a6e6c9bc560654
libi1820 <101002314+libi1820@users.noreply.github.com>
2024-05-08 10:33:03 +0300

    firebase user authentication update

--- app/src/main/java/com/example/sportial/MainActivity.java
+++ app/src/main/java/com/example/sportial/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.sportial;
 
 import android.content.Intent;
+import android.widget.Toast;
+import androidx.annotation.NonNull;
+import android.util.Log;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
